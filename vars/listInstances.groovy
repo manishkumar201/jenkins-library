c246b48d281b3c8af89cb9d8e10075d6ba102b6e
manishkumar201 <122973614+manishkumar201@users.noreply.github.com>
2023-11-22 16:09:12 +0530

    Create listInstances.groovy

---
+++ vars/listInstances.groovy
@@ -0,0 +1,11 @@
+List call(){
+  def command = '''aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text'''
+  def process = command.execute()
+  process.waitFor()
+
+  def instanceIds = []
+  process.text.eachLine { line ->
+    instanceIds.addAll(line.tokenize())
+  }
+  return instanceIds
+}
